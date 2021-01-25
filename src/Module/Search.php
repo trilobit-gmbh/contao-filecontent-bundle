28c7c664fcf780eac0591cfcce00b67ee8510497
Peter Adelmann <git@mineralwa.de>
2021-01-25 17:04:28 +0100

    add search hook method

---
+++ src/Module/Search.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @copyright  trilobit GmbH
+ * @author     trilobit GmbH <https://github.com/trilobit-gmbh>
+ * @license    LGPL-3.0-or-later
+ * @link       http://github.com/trilobit-gmbh/contao-filecontent-bundle
+ */
+
+namespace Trilobit\FilecontentBundle\Module;
+
+use Trilobit\FilecontentBundle\Element\ContentFiles;
+
+/**
+ * Class Search
+ * @package Trilobit\FilecontentBundle\Module
+ */
+class Search extends \Contao\Search
+{
+    /**
+     * @param $content
+     * @param array $data
+     * @param $set
+     */
+    public static function updateIndexPage($content, array $data, &$set)
+    {
+        $set['url'] = str_replace(ContentFiles::$slug[1].'=', ContentFiles::$slug[0].'=', $set['url']);
+    }
+}
